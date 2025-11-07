3e0783930716df40064ef51cff9ad93e355b8077
khoividz <khoivi806@gmail.com>
2025-11-07 19:48:04 +0700

    Delete codedao directory

--- codedao/main.js
+++
@@ -1,8 +0,0 @@
-const gallery = document.getElementById("galleryContainer");
-
-for (let i = 1; i <= 21; i++) {
-const img = document.createElement("img");
-img.src = `img${i}.jpg`;
-img.alt = `Kỷ niệm ${i}`;
-gallery.appendChild(img);
-}
